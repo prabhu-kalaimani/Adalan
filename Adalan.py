ea8aeb1b2f89913faab0680d1673f9700df5821a
Prabhu Kalaimani <prabhu.mc2@gmail.com>
2023-02-16 10:53:04 -0500

    Adding updated files

--- Adalan.py
+++ Adalan.py
@@ -1,12 +1,7 @@
-import time
-
-from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QDialogButtonBox, QHBoxLayout, QLabel
-from PyQt6.QtGui import QIntValidator, QDoubleValidator, QMovie
+from PyQt6.QtWidgets import QApplication, QMainWindow, QDialog, QMessageBox
+from PyQt6.QtGui import QIntValidator, QDoubleValidator, QMovie, QIcon, QPixmap
 from PyQt6.uic import loadUi
 from PyQt6.QtCore import QTimer
-from PySide6.QtCharts import QChart, QChartView, QPieSeries
-from PySide6.QtGui import QPainter, QPen
-from PySide6.QtCore import Qt
 import pyqtgraph as pg
 import random
 import os
@@ -16,6 +11,9 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
         loadUi("Adalan.ui", self)
+        #self.setFixedSize(281, 251)
+        self.setMaximumWidth(self.width())
+        self.setMaximumHeight(self.height())
         # Global variables
         self.slider_max = 100
         self.slider_min = 0
@@ -30,6 +28,11 @@ class MainWindow(QMainWindow):
         self.movie = None
         self.passed_gif = None
         self.failed_gif = None
+        self.answer_response_time = []
+        self.question_index = []
+
+        # Icon
+        self.setWindowIcon(QIcon('adalan_icon.png'))
 
         # Initialization the UI components
         self.chk_add.setChecked(True)
@@ -62,7 +65,7 @@ class MainWindow(QMainWindow):
         self.int_range.setMaximum(self.slider_max)
         self.slider_position = int(self.slider_max / 2)
         self.int_range.setValue(self.slider_position)
-        self.lbl_upper_limt.setText(str(self.slider_max))
+        self.lbl_upper_limt.setText(str(self.int_range.value()))
         self.lbl_lower_limit.setText(str(self.slider_min))
 
         # Total Questions slider
@@ -82,10 +85,42 @@ class MainWindow(QMainWindow):
         self.btn_start.clicked.connect(self.set_test_status_btn)
         self.dial_delay.valueChanged.connect(self.set_dial_text)
         self.inp_total_question.valueChanged.connect(self.update_total_questions)
+        self.menu_howto.triggered.connect(self.how_to)
+        self.menu_about.triggered.connect(self.about_adalan)
+        self.menu_req.triggered.connect(self.menu_requirements)
 
         # Default options for ui
         self.disable_user_input()  # disable user inputs
         self.update_gifs()
+
+    def menu_requirements(self):
+        message = "Windows 10 OS\n" \
+                  "Python 3.8+\n" \
+                  "and a smiling face :)"
+        QMessageBox.about(self, "Requirements", message)
+
+
+    def how_to(self):
+        message = "To use adalan follow the steps\n\n" \
+                  "1. Set the numbers range to fix the min and max range.\n" \
+                  "This is used to randomly pick numbers between the min max value. The default value is 50\n" \
+                  "2. Set the total questions for the test. Default value is 10 and you can choose up to 100 questions\n" \
+                  "3. Set the delay by rotating the knob. This is the time in which you need to answer the question" \
+                  "4. Click the start button to start the test\n" \
+                  "5. Once you click the start button the timer will start decrementing and you need to answer before the" \
+                  "time reaches 0s.The answer is validated and you know the results with the funny jif :) \n\n" \
+                  "Tip: You can use the enter key to validate the answer or after 0 seconds it will auto eveluate your results." \
+                  "\n\n isn't it FUNNY ......"
+        QMessageBox.about(self, "Using Adalan Application", message)
+
+    def about_adalan(self):
+        message = "Adalan is an application developed by Prabhu Kalaimani.\n" \
+                  "It is a fun application to learn mathematics. \nIt helps childrens to" \
+                  "shrapen their basic mathematics skills. \nThis application is tested by Shraven Prabu\n" \
+                  "This application is built and tested on windows platform.\n\nFor any queries improvements please e-mail\n" \
+                  "prabhu_tigers@yahoo.com"
+
+        QMessageBox.about(self, "About Adalan", message)
 
     def update_gifs(self):
         self.passed_gif = ([file for file in os.listdir('gifs/correct_gif') if file.endswith('.gif')])
@@ -103,7 +138,6 @@ class MainWindow(QMainWindow):
         self.movie.start()
 
     def stop_image(self):
-        print("Clearing image")
         self.lbl_disp.clear()
 
     def set_dial_text(self):
@@ -116,7 +150,7 @@ class MainWindow(QMainWindow):
             self.operator_list.append(op_name)
         if op_name in self.operator_list and not chkbox.isChecked():
             self.operator_list.remove(op_name)
-        print("List = ", self.operator_list)
+        # print("List = ", self.operator_list)
 
     def set_test_status_btn(self):
         self.start_testing()
@@ -161,29 +195,40 @@ class MainWindow(QMainWindow):
         self.lbl_operator.hide()
 
     def start_testing(self):
-        self.lbl_ans_status.hide()
-        self.lock_ui()
-        self.show_controls()
-        self.timer.start(1000)
-        self.stop_image()
-        self.enable_user_input()
-        self.count = self.dial_delay.value()
-        self.lbl_timer.setText(str(self.count) + ' s')
-        self.start = True
-        print("Test in progress...")
-
-        x = self.get_random(self.slider_min, self.slider_position)
-        y = self.get_random(self.slider_min, self.slider_position)
-
         if len(self.operator_list) == 0:
-            print("Throw error to select at least one operator")
+            # print("Throw error to select at least one operator")
+            QMessageBox.critical(self, "No operator is chosen !!!", "Chose at least one operator")
         else:
-            operator = self.operator_dict[random.choice(self.operator_list)]
-            # Handle division
-            if operator == "/":
-                print("Handling division")
+            self.lbl_ans_status.hide()
+            self.lock_ui()
+            self.show_controls()
+            self.timer.start(1000)
+            self.stop_image()
+            self.enable_user_input()
+            self.count = self.dial_delay.value()
+            self.lbl_timer.setText(str(self.count))
+            self.start = True
+            # print("Test in progress...")
 
-            print(x, operator, y)
+            x = self.get_random(self.slider_min, self.slider_position)
+            y = self.get_random(self.slider_min, self.slider_position)
+            operator = self.operator_dict[random.choice(self.operator_list)]
+
+            # Handle divide by division
+
+            if operator == "/":
+                if x == 0 and y == 0:
+                    x = 1
+                    y = 1
+                res = 1
+                if y == 0:
+                    tmp_val = x
+                    x = y
+                    y = tmp_val
+                res = x * y
+                x = res
+
+            # print(x, operator, y)
             self.inp_1.setText(str(x))
             self.lbl_operator.setText(operator)
             self.inp_2.setText(str(y))
@@ -219,6 +264,14 @@ class MainWindow(QMainWindow):
         completion_txt = f"{tmp_cnt}/{self.inp_total_question.value()}  {per_complete} % Completed "
         self.lbl_completion_status.setText(completion_txt)
 
+        # Fill up the response time
+        time = self.dial_delay.value() - int(self.lbl_timer.text())
+        self.answer_response_time.append(time)
+        self.question_index.append(tmp_cnt)
+        # print("question ", tmp_cnt)
+        # print("response time ", str(time))
+
+
         # First clear x and y
         self.inp_1.clear()
         self.inp_2.clear()
@@ -238,10 +291,10 @@ class MainWindow(QMainWindow):
             correct_ans = x * y
             result_status = (result == correct_ans)
         elif operator == "/":
-            correct_ans = x / y
-            result_status = (result == round(correct_ans,2))
+            correct_ans = round(x / y)
+            result_status = (result == correct_ans)
 
-        print(f"{result_status} {x} {operator} {y} = {result}")
+        # print(f"{result_status} {x} {operator} {y} = {result}")
         if result_status:
             self.total_correct += 1
             self.display_image(result_status)
@@ -267,15 +320,16 @@ class MainWindow(QMainWindow):
 
         if self.total_questions == 0:
             self.reset_ui()
-            dialog = ShowResults(self.total_correct, self.total_wrong)
+            dialog = ShowResults(patent=self)
             dialog.exec()
             self.total_correct = 0
             self.total_wrong = 0
-
-            # Show dialouge
-            # y = [self.total_correct, self.total_wrong]
-            # x = range(1, len(y) + 1)  # when you change y values, x will automatically have the correct x values
-
+            self.lbl_ans_status.hide()
+            self.lbl_completion_status.clear()
+            self.lbl_disp.clear()
+            self.lbl_timer.setText('0')
+            self.txt_errors.clear()
+            self.txt_errors.appendPlainText("Wrong answers\n")
         else:
             self.btn_start.setText("Next Question")
             self.hide_controls()
@@ -289,8 +343,6 @@ class MainWindow(QMainWindow):
         self.lbl_ques_cnt.setText(str(self.total_questions))
         self.lbl_passed_cnt.setText("0")
         self.lbl_failed_cnt.setText("0")
-        self.txt_errors.clear()
-        self.txt_errors.appendPlainText("Wrong answers\n")
 
     def lock_ui(self):
         self.inp_total_question.setEnabled(False)
@@ -308,43 +360,72 @@ class MainWindow(QMainWindow):
                 # making flag false
                 self.start = False
                 # setting text to the label
-                self.lbl_timer.setText("0 s")
+                self.lbl_timer.setText("0")
                 self.validate_result()
 
         if self.start:
             # getting text from count
-            text = str(self.count) + " s"
+            text = str(self.count)
             # showing text
             self.lbl_timer.setText(text)
 
+
 class ShowResults(QDialog):
-    def __init__(self, x, y):
-        QDialog.__init__(self)
-        layout = QHBoxLayout() # create a layout to add the plotwidget to
-        # window = pg.plot()
-        # window.setGeometry(100, 100, 600, 500)
-        # bargraph = pg.BarGraphItem(x=[1, 2], height=[x, y], width=0.6, brush='g')
-        # window.addItem(bargraph)
-        self.series = QPieSeries()
-        self.series.append('Passed', x)
-        self.series.append('Failed', y)
+    """
+    This class is for the plots
+    """
+    def __init__(self, patent=None):
+        super().__init__(patent)
+        self.parent = patent
+        loadUi("Results.ui", self)
+        self.setMaximumWidth(self.width())
+        self.setMaximumHeight(self.height())
 
-        self.slice = self.series.slices()[1]
-        self.slice.setExploded()
-        self.slice.setLabelVisible()
-        self.slice.setPen(QPen(Qt.darkGreen, 2))
-        self.slice.setBrush(Qt.green)
+        # Bar chart - Pass Vs Failed chart
+        self.pass_fail_graph.setTitle(title="Pass Vs Failed")
+        total_questions = self.parent.total_questions
+        total_corrects = self.parent.total_correct
+        total_wrongs = self.parent.total_wrong
+        # Calculate the pass percentage
+        pass_percentage = round((total_corrects / total_questions) * 100)
+        self.setWindowIcon(QIcon('adalan_icon.png'))
+        pixmap = QPixmap('adalan_icon.png')
+        self.ada_icon.setPixmap(pixmap)
+        self.ada_icon.setScaledContents(True)
 
-        self.chart = QChart()
-        self.chart.addSeries(self.series)
-        chart_view = QChartView(self.chart)
-        # self._chart_view.setRenderHint(QPainter.Antialiasing)
+        status_list = ['Failed', 'Passed']
+        ticks = []
+        xval = list(range(1, len(status_list) + 1))
 
-        self.layout.addWidget(chart_view)
+        bargraph = pg.BarGraphItem(x=[1], height=[total_wrongs], width=0.8, brush='red')
+        self.pass_fail_graph.addItem(bargraph)
+        bargraph = pg.BarGraphItem(x=[2], height=[total_corrects], width=0.8, brush='green')
+        self.pass_fail_graph.addItem(bargraph)
 
-        # self.layout.addWidget(self._chart_view) # add the widget to the layout
-        #self.setLayout(self.layout) # and set the layout on the dialog
-        #self.graphWidget.plot(x, y)
+        for i, item in enumerate(status_list):
+            ticks.append((xval[i], item))
+        ticks = [ticks]
+        ax = self.pass_fail_graph.getAxis('bottom')
+        ax.setTicks(ticks)
+
+        self.lbl_total.setText(str(total_questions))
+        self.lbl_correct.setText(str(total_corrects))
+        self.lbl_wrong.setText(str(total_wrongs))
+        # self.lbl_quick.setText("1")
+        self.lbl_pass_percentage.setText(str(pass_percentage)+"%")
+
+        # Plotting response graph
+        time_list = self.parent.answer_response_time
+        ques_index = self.parent.question_index
+        self.lbl_summary.setText(f"Out of {total_questions} questions you have answered {total_corrects} correctly and {total_wrongs} incorrectly.\n"
+                                 f"Your pass percentage is {pass_percentage} %.\n")
+
+        self.response_time_graph.setTitle(title="Response Time")
+        self.response_time_graph.plot(ques_index, time_list)
+        ques_index = []
+        time_list = []
+
+
 
 app = QApplication([])
 win = MainWindow()
